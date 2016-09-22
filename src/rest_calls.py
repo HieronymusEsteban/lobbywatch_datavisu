d270f25646524c1a22bc2206037a157734457726
Lorenz Padberg <lorenz.padberg@gmail.com>
2016-09-22 19:25:53 +0200

    Made some experiments with Pandas and python notebook.

--- src/rest_calls.py
+++ src/rest_calls.py
@@ -15,3 +15,17 @@ def get_parlamentarier(parlamentarier_id=21):
     response = json.load(urllib2.urlopen(
         "http://lobbywatch.ch/de/data/interface/v1/json/table/parlamentarier/aggregated/id/%s" % parlamentarier_id))
     return response
+
+
+def get_all_parlamentarier(max_id=50):
+    all_parlamentarier = []
+    for parlamentarier_id in range(max_id):
+        try:
+            response = get_parlamentarier(parlamentarier_id)
+            if response['success']:
+                all_parlamentarier.append(response['data'])
+
+        except:
+            pass
+    print 'loaded %s parlamentarier' % len(all_parlamentarier)
+    return all_parlamentarier
--- src/tests/test_rest_calls.py
+++ src/tests/test_rest_calls.py
@@ -9,7 +9,7 @@
 # @author: lorenz.padberg@iterativ.ch
 
 import unittest
-from ..rest_calls import get_parlamentarier
+from ..rest_calls import get_parlamentarier, get_all_parlamentarier
 
 
 class TestRestCalls(unittest.TestCase):
@@ -22,3 +22,7 @@ class TestRestCalls(unittest.TestCase):
         k = get_parlamentarier(21)
         print(k['data'])
         self.assertEqual(type(k['data']), dict)
+
+    def test_get_all_parlameteraier_has_data(self):
+        k = get_all_parlamentarier(max_id=10)
+        self.assertEqual(type(k[0]), dict)
